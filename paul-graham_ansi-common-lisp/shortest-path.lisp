333afc3d17c8e3bb51fb510218940f50197312a8
koplec@github.com <koplec@github.com>
2025-07-21 17:44:56 +0900

    簡単なテストを追加した

--- paul-graham_ansi-common-lisp/shortest-path.lisp
+++ paul-graham_ansi-common-lisp/shortest-path.lisp
@@ -1,6 +1,24 @@
 ;; Paul Graham ANSI Common Lisp
 ;; 3.15 Example: Shortest Path
 
+(defvar *test-counter* 0)
+(defvar *test-failures* 0)
+
+(defmacro test-case (label condition)
+  `(progn
+     (incf *test-counter*)
+     (unless ,condition
+       (incf *test-failures*)
+       (format t "~&[FAILED] ~A~%" ,label))
+     (when ,condition
+       (format t "~%[ OK ] ~A~%" ,label))))
+
+
+;; network-example
+;; (setf my-min '((a b c) (b c) (c d)))
+;; minだと既に定義している名前と被る;; Paul Graham ANSI Common Lisp
+;; 3.15 Example: Shortest Path
+
 ;; network-example
 ;; (setf my-min '((a b c) (b c) (c d)))
 ;; minだと既に定義している名前と被る
@@ -33,32 +51,92 @@
 
 (defun test-new-paths ()
   (let ((test-net '((a b))))
-    ;;aの隣接ノードbから(a)にbを追加した(b a)ができる
-    (assert (equal (new-paths '(a) 'a test-net)
-		   '((b a))))
-    ;;bには隣接ノードがないので結果はnil
-    (assert (equal (new-paths '(b a) 'b test-net)
+    (test-case "aの隣接ノードbから(a)にbを追加した(b a)ができる"
+	       (equal (new-paths '(a) 'a test-net)
+		      '((b a))))
+    (test-case "bには隣接ノードがないので結果はnil"
+	       (equal (new-paths '(b a) 'b test-net)
 		   nil))
-    ;;存在しないノードに対しても結果はnil
-    (assert (equal (new-paths '(b a) 'x test-net)
-		   nil))
-    )
+    (test-case "存在しないノードに対しても結果はnil"
+	       (equal (new-paths '(b a) 'x test-net)
+		   nil)))
 
 
   (let ((test-net-2 '((a b) (b c))))
-    ;;aの隣接ノードbから(a)にbを追加した(b a)ができる
-    (assert (equal (new-paths '(a) 'a test-net-2) '((b a))))
-    ;;bの隣接ノードcから(b a)にcを追加した(c b a)ができる
-    (assert (equal (new-paths '(b a) 'b test-net-2) '((c b a))))
-    ;;隣接ノードがないときは何もできない
-    (assert (equal
-	     (new-paths '(b a) 'x test-net-2)
-	     nil))
+    (test-case "aの隣接ノードbから(a)にbを追加した(b a)ができる"
+	       (equal (new-paths '(a) 'a test-net-2) '((b a))))
+    (test-case "bの隣接ノードcから(b a)にcを追加した(c b a)ができる"
+	       (equal (new-paths '(b a) 'b test-net-2) '((c b a))))
+    (test-case "隣接ノードがないときは何もできない"
+	       (equal
+		(new-paths '(b a) 'x test-net-2)
+		nil))
+    (test-case "すでに(b a)があるときに、aの隣接ノードも追加できそう。。。"
+	       (equal (new-paths '(b a) 'a test-net-2)
+		      '((b b a))))
+    )
+  
+  (format t "*** test-new-paths end ***")
+  t)
+
+
+(defun test-bfs ()
+  (let ((test-net '((a b))))
+    (test-case "a->a"
+	       (equal (bfs 'a '((a)) test-net)
+		      '(a)))
+    (test-case "a->b"
+	       (equal (bfs 'b '((a)) test-net)
+		      '(a b)))
+    )
+
+  (let ((test-net '((a b) (b c))))
+    (test-case "a->a"
+	       (equal (bfs 'a '((a)) test-net)
+		      '(a)))
+    (test-case "a->b"
+	       (equal (bfs 'b '((a)) test-net)
+		      '(a b)))
+    (test-case "a->b->c"
+	       (equal (bfs 'c '((a)) test-net)
+		      '(a b c)))
+
+    )
+
+  (let ((test-net '((a b) (b c d) (d e))))
+    (test-case "a->b->c"
+	       (equal (bfs 'c '((a)) test-net)
+		      '(a b c)))
+    (test-case "a->b->d"
+	       (equal (bfs 'd '((a)) test-net)
+		      '(a b d)))
+    (test-case "a->b->d->e"
+	       (equal (bfs 'e '((a)) test-net)
+		      '(a b d e)))
+
+    (test-case "b->d->e"
+	       (equal (bfs 'e '((b)) test-net)
+		      '(b d e)))
+   
     )
 
 
-  
-  (format t "*** test-new-paths passed ***")
+  (format t "*** test-bfs end ***")
+  t)
+
+(defun test-shortest-path ()
+  (let ((net '((a b c) (b c) (c d))))
+    (test-case "a->c->d"
+	       (equal '(a c d)
+		      (shortest-path 'a 'd net)))
+    (test-case "a->c"
+	       (equal '(a c)
+		      (shortest-path 'a 'c net)))
+     )
+
+  (format t "*** test-shortest-path end ***")
   t)
 
 (test-new-paths)
+(test-bfs)
+(test-shortest-path)
