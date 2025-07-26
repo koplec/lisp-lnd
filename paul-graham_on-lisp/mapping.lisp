02bb70e53e586b7385b2a3b7a24cd277e77d066b
koplec@github.com <koplec@github.com>
2025-07-26 15:19:53 +0900

    on-lisp 4.5読み始め

---
+++ paul-graham_on-lisp/mapping.lisp
@@ -0,0 +1,44 @@
+;; 4.5 mapping
+
+;; 図4.6 map系関数
+
+(defun map0-n (fn n)
+  (mapa-b fn 0 n))
+
+(defun map1-n (fn n)
+  (mapa-b fn 1 n))
+
+(defun mapa-b (fn a b &optional (step 1))
+  "引数aからはじまって、bに至る直前までfnを適用して、その結果の一覧をListで返す"
+  (do ((i a (+ i step))
+       (result nil))
+      ((> i b) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun map-> (fn start test-fn succ-fn)
+  (do ((i start (funcall succ-fn i))
+       (result nil))
+      ((funcall test-fn i) (nreverse result))
+    (push (funcall fn i) result)))
+
+(defun mappend (fn &rest lsts)
+  "非破壊的なmapcan?"
+  (apply #'append (apply #'mapcar fn lsts)))
+
+(defun mapcars (fn &rest lsts)
+  (let ((result nil))
+    (dolist (lst lsts)
+      (dolist (obj lst)
+	(push (funcall fn obj) result)))
+    (nreverse result)))
+
+(defun rmapcar (fn &rest args)
+  (if (some #'atom args)
+      (apply fn args)
+      (apply #'mapcar
+	     #'(lambda (&rest args)
+		 (apply #'rmapcar fn args))
+	     args)))
+
+(defun our-mapcan (fn &rest lsts)
+  (apply #'nconc (apply #'mapcar fn lsts)))
